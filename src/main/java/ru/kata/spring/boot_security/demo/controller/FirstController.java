5b212ceb8cd730442d03844c0cae3e41e825be7b
Desno <desno31@gmail.com>
2023-06-10 12:28:47 +0300

    переименовал название первых страниц

--- src/main/java/ru/kata/spring/boot_security/demo/controller/FirstController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/FirstController.java
@@ -2,18 +2,9 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.kata.spring.boot_security.demo.model.Role;
-import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
-
-import java.util.List;
 
 @Controller
 public class FirstController {
@@ -27,7 +18,7 @@ public class FirstController {
 
     @GetMapping("/admin")
     public String test() {
-        return "Jquery";
+        return "admin";
     }
 
 
@@ -39,7 +30,7 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String starterPage() {
-        return "Jquery";
+        return "admin";
     }
 
 }
